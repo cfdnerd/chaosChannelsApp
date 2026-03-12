becb3cc8ee9e64ea5c43e4e3fa7011dfc979527f
Tom <tomathew.in@gmail.com>
2026-03-12 18:44:44 +0530

    update and debug

--- turbulenceOptimizer/src/diff.c
+++ turbulenceOptimizer/src/diff.c
@@ -4,16 +4,23 @@ scalar diff(volScalarField &gamma,const scalarField &V,double del,double eta,int
      int i;
      scalar z=0;
      double *x =new double[n];
+     const scalar etaEps(1e-6);
+     const scalar etaSafe = Foam::min(Foam::max(eta, etaEps), scalar(1.0)-etaEps);
+     const scalar etaDenominator = Foam::max(etaSafe, etaEps);
+     const scalar oneMinusEtaDenominator = Foam::max(scalar(1.0)-etaSafe, etaEps);
      
      for(i=0;i<n;i++)
      {
-        if(gamma[i]<=eta)
+        const scalar gammaClamped = Foam::min(Foam::max(gamma[i], scalar(0.0)), scalar(1.0));
+        if(gammaClamped<=etaSafe)
         {
-          x[i]=eta*(Foam::exp(-del*(1-gamma[i]/eta))-(1-gamma[i]/eta)*Foam::exp(-del));
+          const scalar ratio = gammaClamped/etaDenominator;
+          x[i]=etaSafe*(Foam::exp(-del*(1-ratio))-(1-ratio)*Foam::exp(-del));
         }
         else
         {
-          x[i]=eta+(1-eta)*(1-Foam::exp(-del*(gamma[i]-eta)/(1-eta))+(gamma[i]-eta)*Foam::exp(-del)/(1-eta));
+          const scalar ratio = (gammaClamped-etaSafe)/oneMinusEtaDenominator;
+          x[i]=etaSafe+(1-etaSafe)*(1-Foam::exp(-del*ratio)+ratio*Foam::exp(-del));
         }
      }
      for(i=0;i<n;i++)
