a0a5bf1bc988a6c832c34a4e1644299436107d62
Tom <tomathew.in@gmail.com>
2026-03-14 02:04:57 +0530

    delete the dumbhead Codex files!

--- turbulenceOptimizer/app/batchRun.sh
+++
@@ -1,6 +0,0 @@
-./Allclean
-blockMesh
-decomposePar
-sbatch jobAzure.sh
-#NPROCS=$(awk '/numberOfSubdomains/{gsub(";","",$2); print $2}' system/decomposeParDict)
-#/shared/home/azureuser/lib/openfoam/mto/ThirdParty-6/platforms/linux64Gcc/openmpi-1.10.7/bin/mpirun -np "$NPROCS" MTO_Turbulence -parallel > log &
--- turbulenceOptimizer/nso/wMake.sh
+++
@@ -1,7 +0,0 @@
-git pull
-cd ../src/
-rm -rf Make/linux*
-wmake -j
-cd ../app/
-git pull
-# for copying purposes: wmake -j 2>&1 | tee build.log
